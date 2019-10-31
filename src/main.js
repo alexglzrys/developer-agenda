a8de89e46efec68537363fd8b7d54c7d05e4ea5a
Alejandro Gonzalez Reyes <jsconestilo@gmail.com>
2019-10-31 14:48:21 -0600

    Dividir app en componentes (App,  Week, Entry)

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  // Por defecto, la instancia principal de Vue (raiz) se le asigna el nombre de Root
+  name: 'Agenda',
   render: h => h(App),
 }).$mount('#app')
